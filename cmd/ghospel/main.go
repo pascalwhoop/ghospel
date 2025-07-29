01eb39f3ad303b551ba92f4ce60ac6ebbd4c2b32
Pascal Brokmeier <git@pascalbrokmeier.de>
2025-07-29 21:15:57 +0200

    test ci for macos as well

--- cmd/ghospel/main.go
+++ cmd/ghospel/main.go
@@ -7,8 +7,16 @@ import (
 	"github.com/pascalwhoop/ghospel/internal/cli"
 )
 
+// Version information injected at build time by GoReleaser
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
 	app := cli.NewApp()
+	app.Version = version
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
